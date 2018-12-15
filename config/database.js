30b6af54cd5b02994c737259792d982d1f24622f
Mari Gameo <mariappangameo@gmail.com>
2018-12-15 10:19:55 +0530

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports ={
+	google:{
+		clientID:'939083327813-ubg1bvcbsasclvf7aq0p08i3jmdqj8o3.apps.googleusercontent.com',
+		clientSecret:'JzMPzORlnq3aZ7F3CUkf6E9n'
+	},
+	mongodb:{
+		dbURI:'mongodb://netninja:zoho1gameo@ds247270.mlab.com:47270/netninja'
+	},
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+}
